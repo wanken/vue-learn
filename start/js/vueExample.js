f45db13b25ed374267b9c6e4653f6a55ab05cd44
waver <jokerwaver@gmail.com>
2019-02-24 22:57:19 +0800

    Fix jshint Warning

--- start/js/vueExample.js
+++ start/js/vueExample.js
@@ -1,11 +1,11 @@
 // 我们的数据对象
 var data = {
-    a: 1
+  a: 1
 };
 
 // 该对象被加入到一个 Vue 实例中
 var vm = new Vue({
-    data: data
+  data: data
 });
 
 // 获得这个实例上的属性
@@ -22,28 +22,28 @@ console.log(vm.a);
 
 // 使用 Object.freeze()， 这会阻止修改现有的属性， 也意味着响应系统无法再追踪变化。
 var obj = {
-    foo: 'bar'
+  foo: "bar"
 };
 
 Object.freeze(obj);
 
 var app = new Vue({
-    el: '#app',
-    data: obj
+  el: "#app",
+  data: obj
 });
 
 var data1 = {
-    a: 1
+  a: 1
 };
 var app1 = new Vue({
-    el: '#app-1',
-    data: data1
+  el: "#app-1",
+  data: data1
 });
 
-app1.$data === data1;
-app1.$el === document.getElementById('app-1');
-app1.$watch('a', function (newValue, oldValue) {
-    console.log("app1的元素内容变为:" + app1.$data.a);
+console.log(app1.$data === data1);
+console.log(app1.$el === document.getElementById("app-1"));
+app1.$watch("a", function(newValue, oldValue) {
+  console.log("app1的元素内容变为:" + app1.$data.a);
 });
 
 //TODO Learn实例生命周期钩子
